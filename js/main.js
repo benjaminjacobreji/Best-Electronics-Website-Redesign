bbaacb3a9db4664b632926c7f61b1d9bacb25c77
Benjamin Jacob Reji <neobenjamin737@gmail.com>
2019-02-18 12:34:49 +0400

    Alpha 0.1

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function returnMenu() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function returnMenuoff() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  
+  function on() {
+    document.getElementById("overlay1").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay1").style.display = "none";
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
